becb406a4d46080c6ed5f91d368c0d311ba23aea
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-17 21:06:26 +0530

    feat: param matching

---
+++ src/params/provider.ts
@@ -0,0 +1,6 @@
+import type { ParamMatcher } from "@sveltejs/kit"
+import type { Provider } from "@supabase/supabase-js"
+
+const PROVIDERS: Provider[] = [ 'github' ]
+
+export const match = ((param: string): param is Provider => PROVIDERS.includes(param as Provider)) satisfies ParamMatcher
--- src/routes/auth/oauth/[service=provider]/+server.ts
+++ src/routes/auth/oauth/[service=provider]/+server.ts
@@ -1,15 +1,11 @@
-import { error, redirect,  } from "@sveltejs/kit"
+import { error, redirect } from "@sveltejs/kit"
 import type {RequestHandler} from "./$types"
-import type { Provider } from "@supabase/supabase-js"
-
-const PROVIDERS: Provider[] = [ 'github' ]
 
 export const GET: RequestHandler = async ({ params: { service }, url, locals: { supabase }}) => {
-  if (!PROVIDERS.includes(service as Provider)) error(404, "Not Found")
   const next = url.searchParams.get('next')
   const nextUrl = new URL(next || '/', url.origin)
   const { data, error: oauthErr } = await supabase.auth.signInWithOAuth({
-    provider: service as Provider,
+    provider: service,
     options: {
       queryParams: {
         next: url.origin === nextUrl.origin ? nextUrl.pathname : '/'
