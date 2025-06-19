e4328e3f045d66e0424d6c711e66ceedba447e4c
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-19 15:14:21 +0530

    fix: auth guard

---
+++ src/routes/app/+layout.ts
@@ -0,0 +1,13 @@
+import type { LayoutLoad } from "./$types"
+import { redirect } from "@sveltejs/kit";
+
+export const load: LayoutLoad = async ({ parent }) => {
+  const data = await parent();
+  if (!data.auth.session || !data.auth.user) redirect(303, "/auth");
+  // doing this destructuring to ensure that typescript infers that
+  // data.auth.(session|user) are not null.
+  return {
+    supabase: data.supabase,
+    auth: data.auth
+  }
+}
