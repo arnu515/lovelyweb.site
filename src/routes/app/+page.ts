85b674e77674bab0d31edb4071b317cdbfef437f
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-22 17:40:28 +0530

    fix:

--- src/routes/app/+page.ts
+++ src/routes/app/+page.ts
@@ -5,10 +5,11 @@ import { captureException } from "@sentry/sveltekit";
 export const load: PageLoad = async ({ parent }) => {
   const data = await parent();
 
-  const {data: org, error: orgError} = await data.supabase.from("organisations").select("id").limit(1).maybeSingle()
+  const {data: org, error: orgError} = await data.supabase.from("organisations_users").select("organisation_id", { count: 'exact', head: true }).eq("user_id", data.auth.user.id).limit(1).maybeSingle()
   if (orgError) {
     captureException(orgError, { tags: { supabase: 'organisations' }})
     error(500, { message: orgError.message });
   }
-  if (org) redirect(303, '/app/' + org.id)
+  if (org) redirect(303, '/app/' + org.organisation_id)
+  else redirect(303, '/app/inv')
 }
