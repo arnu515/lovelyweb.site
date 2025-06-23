cf29ef2fdc9fd3ea61137d8d2a1c5c58d3a0d21d
arnu515 <52203828+arnu515@users.noreply.github.com>
2025-06-23 16:47:01 +0530

    fix: add auth redirect

--- src/routes/app/[orgId]/chat/+layout.ts
+++ src/routes/app/[orgId]/chat/+layout.ts
@@ -1,6 +1,8 @@
+import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ parent }) => {
   const data = await parent();
+  if (!data.auth.session) redirect(303, '/auth')
   return data;
 };
